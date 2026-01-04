b91cc9472b4de0964a2388f16a171660a95e5159
Radeusz32 <radeusz32@gmail.com>
2026-01-04 13:39:45 +0100

    app structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  alias: {
+    "@ui": "/app/components/ui",
+    "@animated": "/app/components/animated",
+    "@features": "/app/features",
+    "@services": "/app/services",
+    "@composables": "/app/composables",
+    "@types": "/app/types",
+    "@utils": "/app/utils",
+  },
+});
